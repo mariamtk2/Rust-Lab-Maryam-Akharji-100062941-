3f43efdde37d31cf5e526c192018897ad7075e15
Maryam Alkharji 100062941 <165850624+mariamtk2@users.noreply.github.com>
2025-04-07 09:24:50 +0400

    Create iterators4.rs

---
+++ 18_iterators/iterators4.rs
@@ -0,0 +1,37 @@
+fn factorial(num: u64) -> u64 {
+        let mut result = 1;
+    
+        for x in 2..=num {
+            result *= x;
+        }
+    
+        result
+    }
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(factorial(1), 1);
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(factorial(4), 24);
+    }
+}
